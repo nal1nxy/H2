dd6b86be38e522dcd1fc23e84e8cee682f8e1ac7
NalinYet <nalin.yetukuri@unh.edu>
2024-10-14 14:00:04 -0400

    Test functions written for the method get_freq() in test_get_freq.py

--- test_get_freq.py
+++ test_get_freq.py
@@ -3,3 +3,30 @@ File: test_get_freq.py
 Initial developers: COMP 801 instructors
 Developer: Nalin Yetukuri
 """
+import pytest
+from scrabble_letters import ScrabbleLetters
+
+
+def test_get_freq_1():
+    """
+    Test the method by giving a word with a blank space as input
+    """
+    word = "a c"
+    test_word = ScrabbleLetters(word)
+    actual = test_word.scrabble_letters
+    expected = {'a': 9, ' ': 0, 'c': 2}
+    assert actual == expected
+
+
+def test_get_freq_2():
+    """
+    test the method by giving a word with letters in upper case
+    """
+    word = "ABC"
+    test_word = ScrabbleLetters(word)
+    actual = test_word.scrabble_letters
+    expected = {'a': 9, 'b': 2, 'c': 2}
+    assert actual == expected
+
+
+pytest.main()
